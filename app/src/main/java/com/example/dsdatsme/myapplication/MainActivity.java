bad07266ba6cff756ccf3b0b0ec040939dd2c4eb
DSdatsme <darsuratwala@gmail.com>
2018-03-03 02:10:08 +0530

    Version 0.1

--- app/src/main/java/com/example/dsdatsme/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dsdatsme/myapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.dsdatsme.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private String url = "www.udacity.com";
     private String phoneNumber = "650555555";
     Uri maps = Uri.parse("geo:37.4001293,-122.1105186?q=Udacity,+Latham,+CA");
@@ -14,8 +17,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+
 
     public void openMapsIntent(View view) {
 
